b1d2c35ea1f5ab005e73f4e1169cb27aeda22be9
andreviniciup <167660879+andreviniciup@users.noreply.github.com>
2025-04-14 20:46:38 -0300

    Adicionar indicador de progresso para upload de arquivos com spinner e feedback de processamento

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from app import create_app
-from config import FLASK_PORT, DEBUG
+from config import FLASK_PORT 
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=DEBUG, port=FLASK_PORT)
+    app.run(debug=True, port=FLASK_PORT)
